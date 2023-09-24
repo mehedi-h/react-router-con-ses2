79c639368a8d4a1121441dcbe9ff147d44afc3ac
mehedi-h <mehedihs2015@gmail.com>
2023-09-24 20:48:46 +0600

    toast Added

--- src/pages/PhoneDetails/PhoneCard.jsx
+++ src/pages/PhoneDetails/PhoneCard.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import swal from 'sweetalert';
 
 const PhoneCard = ({phone}) => {
-    console.log(phone);
+    // console.log(phone);
     const {id, phone_name, brand_name, rating, price, image} = phone || {};
+
+    const handleAddFav = () => {
+        // console.log(phone);
+        const addToFavourite = [];
+
+        const getSavedPhones = JSON.parse(localStorage.getItem('favourites'));
+
+        // jodi getSavedPhone e kisu na thake
+        if (!getSavedPhones) {
+            addToFavourite.push(phone)
+            localStorage.setItem('favourites', JSON.stringify(addToFavourite));
+            swal("Good job!", "Product Added Successfully!", "success");
+        }
+        
+        else {
+            const isExists = getSavedPhones.find(phone => phone.id === id)
+
+            if (!isExists) {
+                addToFavourite.push(...addToFavourite, phone);
+                localStorage.setItem('favourites', JSON.stringify(addToFavourite))
+                swal("Good job!", "Product Added Successfully!", "success");
+            }
+            else {
+                swal("Already Exist!", "No Duplicate Added!", "error");
+            }
+            
+        }
+
+        // localStorage.setItem("favourites", JSON.stringify([{name: 'batashi'}, {prof: 'nursing'}]))
+    }
     
 
     return (
-        <div className="md:pt-12 lg:pt-20">
+        <div className="flex justify-center items-center h-[70vh]">
             <div className="relative flex w-full max-w-[48rem] flex-row rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
                 <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-slate-500 bg-clip-border text-gray-700">
                     <img
@@ -27,6 +58,7 @@ const PhoneCard = ({phone}) => {
     
                     <a className="inline-block" href="#">
                         <button
+                            onClick={handleAddFav}
                             className="flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                             type="button">
                             Add to Favourites
